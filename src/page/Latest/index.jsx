4cfc689a2267a11b6460e76149febdf408952f5e
wlihaopeng_v <wlihaopeng_v@didiglobal.com>
2023-08-23 20:08:41 +0800

    markdown解析器（1%）

--- src/page/Latest/index.jsx
+++ src/page/Latest/index.jsx
@@ -1,6 +1,28 @@
+import { Input } from "antd"
+import { useEffect, useState } from "react"
 
 const Latest = () => {
-    return <div className="latest">最新</div>
+
+    const [value, setValue] = useState('')
+    return <div className="latest" style={{display: 'flex', height: '100%'}}  >
+        <div className="left" style={{height: '100%', width: '50%', border: '1px solid #000'}} >
+            <Input.TextArea style={{height: '100%'}} onChange={event => setValue(event.target.value)} />
+        </div>
+        <div className="right" style={{height: '100%', width: '50%', border: '1px solid #000'}} >
+            <RenderHtml data={value} />
+        </div>
+    </div>
+}
+
+const RenderHtml = ({data = ''}) => {
+    useEffect(() => {
+        if(data) {
+
+        }
+    }, [data])
+    return <div className="renderHtml">
+        {data}
+    </div>
 }
 
 export default Latest
